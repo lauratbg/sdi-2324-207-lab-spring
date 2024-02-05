e82bad2442ffc7db175b2b337b9e8f53fa700a26
lauratbg <UO275725@uniovi.es>
2024-02-05 16:43:03 +0100

    SDI-2324-207-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2324207spring/Sdi2324207SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324207spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324207SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324207SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2324207spring/Sdi2324207SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324207spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324207SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
